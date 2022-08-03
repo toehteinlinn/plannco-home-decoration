0c9d098bfa468a1966f2705bb2550825644d53a9
toehteinlinn <hteinlinn2002@gmail.com>
2022-08-03 22:55:21 +0630

    planncohomedecoration

---
+++ js/app.js
@@ -0,0 +1,137 @@
+$(document).ready(function(){
+
+
+    // Start Back to Top
+    $(".btn-backtotops").hide();
+    $(window).scroll(function(){
+        let getscrolltop = $(this).scrollTop();
+        // console.log(getscrolltop);
+
+        if(getscrolltop >= 370){
+            $(".btn-backtotops").fadeIn(1000);
+        }else{
+            $(".btn-backtotops").fadeOut(1000);
+        }
+    });
+    // End Back to Top
+
+
+
+    // Start Header
+
+        // Start nav
+
+            // For bugger
+            $(".navbuttons").click(function(){
+                $(".navbuttons").toggleClass("crossxs");
+            });
+
+            // For nav
+            $(window).scroll(function(){
+
+                let getscrolltop = $(this).scrollTop();
+                console.log(getscrolltop);
+
+                if(getscrolltop >= 200){
+                    $(".navbar").addClass("navmenus");
+                }else{
+                    $(".navbar").removeClass("navmenus");
+                }
+            });
+
+        // End nav
+
+    // End Header
+
+
+
+
+
+
+    // Start properties
+
+    $(".propertylists").click(function(){
+
+        // for acitve items
+        // $(this).addClass("activeitems");
+        // $(this).siblings().removeClass("activeitems");
+        $(this).addClass("activeitems").siblings().removeClass("activeitems");
+
+
+        // for filter
+        let getattvalue = $(this).attr("data-filter");
+        console.log(getattvalue);
+
+        if(getattvalue === "all"){
+            $(".filters").show("slide",500);
+        }else{
+
+            $(".filters").hide();
+            
+            $(".filters").not("."+getattvalue).hide("slide",500);
+
+            $(".filters").filter("."+getattvalue).show("slide",500);
+        }
+
+    });
+
+
+    // For image overlay (or) lightbox2
+    lightbox.option({
+        showImageNumberLabel:false
+    });
+
+    // End properties section
+
+
+
+
+    // Start Adv section
+
+    $(window).scroll(function(){
+        let getscrolltop = $(this).scrollTop();
+
+        // console.log(getscrolltop);
+
+        if(getscrolltop >= 900){
+            $(".advimages").addClass("fromlefts");
+            $(".advtexts").addClass("fromrights");
+        }else{
+            $(".advimages").removeClass("fromlefts");
+            $(".advtexts").removeClass("fromrights");        }
+    });
+    
+    // End Adv Section
+
+
+
+
+    // Start Footer Section
+    const getyear = $("#getyear");
+    const getfullyear = new Date().getUTCFullYear();
+    getyear.text(getfullyear);
+    // End Footer Section
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+// 16PL
+// 23PP
+// 30ST
+// 7TM
+// 8MF
+// 14PC
+
+
+
+
